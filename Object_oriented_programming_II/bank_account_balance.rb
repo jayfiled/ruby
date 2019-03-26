ac07018ea9f9c5db2004ecfc46b827948fd83393
jayfiled <jayfiled@gmail.com>
2019-03-26 22:02:15 +1100

    add private methods

--- Object_oriented_programming_II/bank_account_balance.rb
+++ Object_oriented_programming_II/bank_account_balance.rb
@@ -4,4 +4,15 @@ class Account
       @name = name
       @balance = balance
     end
+    
+    private
+    
+    def pin
+      @pin = 1234
+    end
+    
+    def pin_error
+      "Access denied: incorrect PIN."
+    end
+    
   end
